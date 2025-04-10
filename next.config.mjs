fe49bdd374cbf29cbc45517f104836f3b08e6b5b
ronrcruz <124099037+ronrcruz@users.noreply.github.com>
2025-04-10 15:15:19 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
 
-// next.config.js
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
-
 
 export default nextConfig;
